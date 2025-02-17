4d839b996a9e9fc4d820a0301eb6306163957ea5
Alison Estephany <96319139+Alisoneoz@users.noreply.github.com>
2025-02-17 23:19:22 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+
   console.log("Hello Rigo from the console!");
-};
+ };
+
+
+function domainGenerator(){
+    let pronouns = ['the', 'our'];
+
+    let adjs = ['great', 'big'];
+    
+    let nouns = ['jogger', 'racoon'];
+
+    let domainName = []
+
+    for(let i = 0 ; i< pronouns.length ; i++){
+        for (let j = 0 ; j < adjs.length ; j++){
+            for (let k = 0 ; k < nouns.length ; k++){
+               domainName.push(pronouns[i] + adjs[j] + nouns[k]+ ".com") 
+            }
+        }
+    }
+return domainName;
+}
+
+console.log(domainGenerator())
